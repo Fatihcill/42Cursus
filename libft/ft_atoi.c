73abd8ffa6b2bd38b8ea96b49a782a447e9c581c
Fatih Cil <fcil@k2m12s09.42kocaeli.com.tr>
2022-02-04 10:31:02 +0300

    final

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,31 +6,48 @@
 /*   By: fcil <fcil@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 18:57:53 by fcil              #+#    #+#             */
-/*   Updated: 2022/02/03 16:52:43 by fcil             ###   ########.fr       */
+/*   Updated: 2022/02/04 10:30:27 by fcil             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "limits.h"
 
-int	ft_atoi(const char *str)
+static int	ft_isspace(char chr)
 {
-	int	n;
-	int	negative;
-
-	while ((*str >= 9 && *str <= 13) || *str == ' ')
-		str++;
-	negative = 0;
-	if (*str == '-' || *str == '+')
-		if (*str++ == '-')
-			negative = 1 - negative;
-	n = 0;
-	while (*str >= '0' && *str <= '9')
-		n = n * 10 + *str++ - '0';
-	if (negative)
-		n *= -1;
-	return (n);
+	if ((chr >= 9 && chr <= 13) || chr == ' ')
+		return (1);
+	return (0);
 }
 
-//20. satirda bosluklari yok ediyoruz.
-//23 de  - mi + mi kontrol ediyoruz.
-// 28 de basamak basamak rakamlari ekliyoruz. ve return ediyoruz.
+int	ft_atoi(const char *nptr)
+{
+	unsigned long	ret_val;
+	int				index;
+	int				polarity;
+
+	polarity = 1;
+	index = 0;
+	if (*nptr == '\0')
+		return (0);
+	while (ft_isspace(nptr[index]))
+		index++;
+	if (nptr[index] == '-' || nptr[index] == '+')
+	{
+		if (nptr[index] == '-')
+			polarity *= -1;
+		index++;
+	}
+	ret_val = 0;
+	while (nptr[index] >= '0' && nptr[index] <= '9')
+		ret_val = (ret_val * 10) + (nptr[index++] - '0');
+	if (ret_val > LONG_MAX && polarity == -1)
+		return (0);
+	if (ret_val > LONG_MAX && polarity == 1)
+		return (-1);
+	return (ret_val * polarity);
+}
+
+//33. satirda bosluklari yok ediyoruz.
+//35 de  - mi + mi kontrol ediyoruz.
+// 42 de basamak basamak rakamlari ekliyoruz. ve return ediyoruz.
